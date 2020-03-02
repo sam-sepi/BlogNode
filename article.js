b12906b1d199561ce9c942795e55ae029131e1ae
sam-sepi <sam.sepi.84@gmail.com>
2020-03-02 19:14:54 +0100

    db

--- article.js
+++ article.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const Datastore = require('nedb');
+db = new Datastore({ filename: 'articles.db', autoload: true });
+
 router.get('/', (req, res) => 
 {
     res.json({Response: 'Received'});
