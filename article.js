5d887b12e22cb497e08d37e9f00df6e0fb1c63ef
sam-sepi <sam.sepi.84@gmail.com>
2020-03-03 07:55:58 +0100

    body-parser

--- article.js
+++ article.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 const Datastore = require('nedb');
 db = new Datastore({ filename: 'articles.db', autoload: true });
@@ -9,15 +11,12 @@ router.get('/', (req, res) =>
     res.json({Response: 'Received'});
 })
 
-router.post('/', (req, res) =>
+router.post('/', jsonParser, (req, res) =>
 {
     const article = req.body;
     const timestamp = Date.now();
     article.timestamp = timestamp;
-    db.insert(article, (err, newDoc) => 
-    {
-        console.log(err);
-    });
+    db.insert(article);
 
     res.json(article);
 });
